07e249e2d7ec4a93a0ecda572f88b401a96edc3a
Samuel Allen <dehuszar@gmail.com>
2018-05-30 14:34:11 -0400

    initial commit

---
+++ GridContainer.js
@@ -0,0 +1,28 @@
+// defines properties usable by a grid container
+// source: https://css-tricks.com/snippets/css/complete-guide-grid/
+import styled from 'styled-components';
+
+const GridContainer = styled.div`
+  align-content: ${props => props.alignContent};
+  align-items: ${props => props.alignItems};
+  display: ${props => (props.inline ? 'inline-grid' : 'grid')};
+  grid-auto-columns: ${props => props.gridAutoColumns};
+  grid-auto-flow: ${props => props.gridAutoFlow};
+  grid-auto-rows: ${props => props.gridAutoRows};
+  grid-column-gap: ${props => props.gridColumnGap};
+  grid-column-start: ${props => props.gridColumnStart};
+  grid-gap: ${props => props.gridGap};
+  grid-row-gap: ${props => props.gridRowGap};
+  grid-template-areas: ${props => props.gridTemplateAreas};
+  grid-template-columns: ${props => props.gridTemplateColumns};
+  grid-template-columns: ${props => props.gridTemplateColumns};
+  grid-template-rows: ${props => props.gridTemplateRows};
+  grid-template: ${props => props.gridTemplate};
+  grid: ${props => props.grid};
+  justify-content: ${props => props.justifyContent};
+  justify-items: ${props => props.justifyItems};
+  place-content: ${props => props.placeContent};
+  place-items: ${props => props.placeItems};
+`;
+
+export { GridContainer };
---
+++ GridItem.js
@@ -0,0 +1,18 @@
+// defines properties usable by a grid item
+// source: https://css-tricks.com/snippets/css/complete-guide-grid/
+import styled from 'styled-components';
+
+const GridItem = styled.div`
+  align-self: ${props => props.alignSelf};
+  grid-area: ${props => props.gridArea};
+  grid-column-end: ${props => props.gridColumnEnd};
+  grid-column-start: ${props => props.gridColumnStart};
+  grid-column: ${props => props.gridColumn};
+  grid-row-end: ${props => props.gridRowEnd};
+  grid-row-start: ${props => props.gridRowStart};
+  grid-row: ${props => props.gridRow};
+  justify-self: ${props => props.justifySelf};
+  place-self: ${props => props.placeSelf};
+`;
+
+export { GridItem };
